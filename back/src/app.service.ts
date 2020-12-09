e58b5933a1b4e3f4200c6115b24b343512d093ac
Stéphane Francel <contact@avine.io>
2020-12-09 01:10:18 +0100

    back: just try to retrieve all collections from main AppService

--- back/src/app.service.ts
+++ back/src/app.service.ts
@@ -1,8 +1,22 @@
 import { Injectable } from '@nestjs/common';
 
+import { LeaguesService } from './leagues/leagues.service';
+import { PlayersService } from './players/players.service';
+import { TeamsService } from './teams/teams.service';
+
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(
+    private leaguesService: LeaguesService,
+    private teamsService: TeamsService,
+    private playersService: PlayersService,
+  ) {}
+
+  getAll() {
+    return Promise.all([
+      this.leaguesService.findAll(),
+      this.teamsService.findAll(),
+      this.playersService.findAll(),
+    ]);
   }
 }
